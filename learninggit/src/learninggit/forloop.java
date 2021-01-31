04ee3a6f1edc25b9d6ab87447ba5f1116562f6e4
Chetan <mishrachetan001@gmail.com>
2021-01-31 14:24:42 +0530

    for loop modified

--- learninggit/src/learninggit/forloop.java
+++ learninggit/src/learninggit/forloop.java
@@ -1,11 +1,16 @@
 package learninggit;
 
 public class forloop {
+	public static void method() {
+		for(int k=0;k<5;k++) {
+			System.out.println("yahooo");
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("for looop ");
-
+method();
 	}
 
 }
